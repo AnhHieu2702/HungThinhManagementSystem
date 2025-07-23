71e43137ef05a585a81832e6903238db65dc31c2
Anhhieu2702 <vungochieu099(@gmail.com>
2025-07-23 13:42:13 +0700

    rename file

---
+++ src/main/java/com/apartment/models/dtos/staffs/StaffCreateRequest.java
@@ -0,0 +1,16 @@
+package com.apartment.models.dtos.staffs;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class StaffCreateRequest {
+    @NotBlank(message = "Vui long nhập tên đăng nhập")
+    private String username;
+
+    @NotBlank(message = "Vui lòng nhập mật khẩu")
+    private String password;
+
+    @NotBlank(message = "Vui lòng nhập vai trò")
+    private String role;
+}
