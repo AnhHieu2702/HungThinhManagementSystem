6b0e66af766468f9272cd8935b99e1bd4de3600a
Anhhieu2702 <vungochieu099(@gmail.com>
2025-07-23 23:02:41 +0700

    complete for ResidentCRUD

--- src/main/java/com/apartment/services/interfaces/IResidentService.java
+++ src/main/java/com/apartment/services/interfaces/IResidentService.java
@@ -1,10 +1,16 @@
 package com.apartment.services.interfaces;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.apartment.models.dtos.residents.ResidentCreateRequest;
+import com.apartment.models.dtos.residents.ResidentGetsResponse;
 import com.apartment.models.global.ApiResult;
 
 public interface IResidentService {
+    ApiResult<List<ResidentGetsResponse>> getResidentsByApartmentId(UUID apartmentId);
+
     ApiResult<UUID> createResident(UUID apartmentId, ResidentCreateRequest apiRequest);
+
+    ApiResult<String> updateResident(UUID residentId, ResidentCreateRequest apiRequest);
 }
