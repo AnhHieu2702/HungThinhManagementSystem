b191d1885e28674156cdf9b858fb940e9f903ec8
Anhhieu2702 <vungochieu099(@gmail.com>
2025-07-25 17:28:54 +0700

    jwt in swagger

--- src/main/java/com/apartment/models/configuration/OpenApiConfig.java
+++ src/main/java/com/apartment/models/configuration/OpenApiConfig.java
@@ -3,8 +3,11 @@ package com.apartment.models.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 //import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.servers.Server;
 // import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -26,17 +29,17 @@ public class OpenApiConfig {
                 .servers(List.of(
                         new Server()
                                 .url("http://localhost:8080")
-                                .description("Development Server")));
-        // // Thêm cấu hình Authorization
-        // .components(new Components()
-        // .addSecuritySchemes("Bearer Token",
-        // new SecurityScheme()
-        // .type(SecurityScheme.Type.HTTP)
-        // .scheme("bearer")
-        // .bearerFormat("JWT")
-        // .description("Nhập JWT token để xác thực")))
-        // .addSecurityItem(new SecurityRequirement()
-        // .addList("Bearer Token"));
+                                .description("Development Server")))
+                                
+                // Thêm cấu hình Authorization
+                .components(new Components()
+                .addSecuritySchemes("Bearer Token",
+                new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("Nhập JWT token để xác thực")))
+                .addSecurityItem(new SecurityRequirement()
+                .addList("Bearer Token"));
     }
 }
-
