a35c4d581736bb7c42457303966a8701c40b0e4e
td-29th02 <tuandung290204@gmail.com>
2025-07-28 08:49:49 +0700

    change feedback ID to username

--- src/main/java/com/apartment/models/dtos/feedbacks/FeedbackAssignRequest.java
+++ src/main/java/com/apartment/models/dtos/feedbacks/FeedbackAssignRequest.java
@@ -1,12 +1,10 @@
 package com.apartment.models.dtos.feedbacks;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-
-import java.util.UUID;
 
 @Data
 public class FeedbackAssignRequest {
-    @NotNull(message = "Vui lòng chọn người xử lý")
-    private UUID assignedTo;
+    @NotBlank(message = "Vui lòng chọn người xử lý")
+    private String assignedToUsername;
 }
