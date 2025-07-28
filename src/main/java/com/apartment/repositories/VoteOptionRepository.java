a54c5a496ad1511c09f57a6bcc7a7ef5325c9914
trang204 <ngtrang2204@gmail.com>
2025-07-28 15:12:55 +0700

    vote repository with query methods

---
+++ src/main/java/com/apartment/repositories/VoteOptionRepository.java
@@ -0,0 +1,19 @@
+package com.apartment.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apartment.models.entities.bases.VoteOption;
+
+public interface VoteOptionRepository extends JpaRepository<VoteOption, UUID> {
+    
+    List<VoteOption> findByVoteIdOrderByDisplayOrder(UUID voteId);
+    
+    List<VoteOption> findByVoteId(UUID voteId);
+    
+    List<VoteOption> findByVoteIdAndDisplayOrder(UUID voteId, Integer displayOrder);
+    
+    void deleteByVoteId(UUID voteId);
+}
