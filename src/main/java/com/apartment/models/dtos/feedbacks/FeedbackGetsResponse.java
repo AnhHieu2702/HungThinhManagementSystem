f93a9569930461d1eb74989b616a74426b014921
td-29th02 <tuandung290204@gmail.com>
2025-07-28 08:04:33 +0700

    remove createdAt and updatedAt

--- src/main/java/com/apartment/models/dtos/feedbacks/FeedbackGetsResponse.java
+++ src/main/java/com/apartment/models/dtos/feedbacks/FeedbackGetsResponse.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -20,6 +19,4 @@ public class FeedbackGetsResponse {
     private String status;
     private String response;
     private String assignedToName;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 }
