9cb9f29fa562deee5cd57dd642beff5dc78f889a
td-29th02 <tuandung290204@gmail.com>
2025-07-28 07:40:35 +0700

    define IFeedbackService interface with CRUD operations

---
+++ src/main/java/com/apartment/services/interfaces/IFeedbackService.java
@@ -0,0 +1,26 @@
+package com.apartment.services.interfaces;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.apartment.models.dtos.feedbacks.FeedbackAssignRequest;
+import com.apartment.models.dtos.feedbacks.FeedbackCreateRequest;
+import com.apartment.models.dtos.feedbacks.FeedbackGetsResponse;
+import com.apartment.models.dtos.feedbacks.FeedbackUpdateRequest;
+import com.apartment.models.global.ApiResult;
+
+public interface IFeedbackService {
+    ApiResult<List<FeedbackGetsResponse>> getsFeedback();
+    
+    ApiResult<UUID> createFeedback(FeedbackCreateRequest apiRequest, UUID residentId);
+    
+    ApiResult<String> updateFeedback(UUID feedbackId, FeedbackUpdateRequest apiRequest);
+    
+    ApiResult<String> assignFeedback(UUID feedbackId, FeedbackAssignRequest apiRequest);
+    
+    ApiResult<List<FeedbackGetsResponse>> getFeedbacksByResident(UUID residentId);
+    
+    ApiResult<List<FeedbackGetsResponse>> getFeedbacksByStatus(String status);
+    
+    ApiResult<List<FeedbackGetsResponse>> getFeedbacksByAssignedTo(UUID assignedToId);
+}
