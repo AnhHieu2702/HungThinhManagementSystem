d9673e2fc0c5a644a946c51c81ffeddf2c37cdeb
td-29th02 <tuandung290204@gmail.com>
2025-07-30 11:08:11 +0700

    invoice data access

---
+++ src/main/java/com/apartment/repositories/InvoiceRepository.java
@@ -0,0 +1,25 @@
+package com.apartment.repositories;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apartment.models.entities.bases.Invoice;
+import com.apartment.models.entities.enums.InvoiceStatus;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {
+    
+    List<Invoice> findByStatus(InvoiceStatus status);
+    
+    List<Invoice> findByApartmentId(UUID apartmentId);
+    
+    List<Invoice> findByMonthAndYear(Integer month, Integer year);
+    
+    Optional<Invoice> findByApartmentIdAndMonthAndYear(UUID apartmentId, Integer month, Integer year);
+    
+    List<Invoice> findByApartmentApartmentNumber(String apartmentNumber);
+    
+    Optional<Invoice> findByInvoiceNumber(String invoiceNumber);
+}
