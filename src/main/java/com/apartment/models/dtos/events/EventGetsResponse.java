f42a668c5f86bc3dd9d3eabb10719714382bd5b1
trang204 <ngtrang2204@gmail.com>
2025-08-17 22:58:52 +0700

    add response event

--- src/main/java/com/apartment/models/dtos/events/EventGetsResponse.java
+++ src/main/java/com/apartment/models/dtos/events/EventGetsResponse.java
@@ -4,20 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
+
+import groovy.transform.builder.Builder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class EventGetsResponse {
     private UUID id;
     private String title;
     private String description;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
     private String location;
-    private String eventType;
-    private String createdByName;
-    private Boolean isPublic;
 }
